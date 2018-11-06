2fee91c147711502bd40005ff9c287047736d914
mortefal <morten_falstad@hotmail.com>
2018-11-06 18:44:55 +0100

    Added toggle on favoriteHeart #7

--- app/src/App.js
+++ app/src/App.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 
 import SimpleCard from "./Components/SimpleCard";
 import TabBar from "./Components/TabBar";
-import Input from '@material-ui/core/Input';
-import TextField from "@material-ui/core/TextField/TextField";
 import InputBar from './Components/inputBar.js';
-
 class App extends Component {
-    constructor(props) {
-        super(props);
-        };
+
     render() {
     return (
         <div>
             <TabBar/>
             <InputBar/>
-            <SimpleCard title="Dette kan settes med props" description="Det kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her også" pris="OG prisen!" varenummer="10121514425"/>
+            <SimpleCard  title="Dette kan settes med props" description="Det kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her også" pris="267" varenummer="10121514425"/>
+            <SimpleCard  title="Dette kan settes med props" description="Det kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her også" pris="267" varenummer="10121514425"/>
+            <SimpleCard  title="Dette kan settes med props" description="Det kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her også" pris="267" varenummer="10121514425"/>
+            <SimpleCard  title="Dette kan settes med props" description="Det kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her også" pris="267" varenummer="10121514425"/>
+            <SimpleCard  title="Dette kan settes med props" description="Det kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her også" pris="267" varenummer="10121514425"/>
+            <SimpleCard  title="Dette kan settes med props" description="Det kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her ogsåDet kan det her også" pris="267" varenummer="10121514425"/>
         </div>
 
     );
---
+++ app/src/Components/FavoriteHeart.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+import IconButton from '@material-ui/core/IconButton'
+import PropTypes from 'prop-types';
+
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import {withStyles} from "@material-ui/core";
+
+const styles = theme => ({
+    root: {
+        color: "primary",
+    }
+})
+
+class FavoriteHeart extends React.Component {
+    render() {
+        const {isFav, classes} = this.props;
+        return (
+            <IconButton className={classes.root}>
+                {isFav ? <FavoriteIcon style={{color: 'red'}}/>: <FavoriteBorder/>}
+            </IconButton>
+        )
+    }
+}
+FavoriteHeart.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+export default withStyles(styles)(FavoriteHeart);
+
