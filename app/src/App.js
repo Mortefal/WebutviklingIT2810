d8638d3b11ee8e8ab4cfde36cdc53d2c09b03e0b
Ebba Louise Toreld Fingarsen <elfingar@stud.ntnu.no>
2018-11-06 11:07:32 +0100

    working logic for saving previous queries #2

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import input from './Components/inputBar.js'
-import searchexample from './Components/inputBar.js'
+import InputBar from './Components/inputBar.js'
 
 class App extends Component {
   render() {
     return (
-        <inputBar></inputBar>
+        <InputBar/>
     );
   }
 }
--- app/src/Components/inputBar.js
+++ app/src/Components/inputBar.js
@@ -1,34 +1,66 @@
 import React, { Component } from 'react'
 
-
-export default class inputBar extends Component {
+export default class InputBar extends Component{
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {
+            queryId: 0,
+            query: '',
+            previousQueries: [],
+            results:[]
+        };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        this.setState({value: event.target.value});
+        this.setState({query: event.target.value});
+    }
+
+    getInfo(){
+        //now logikk for å hente ut ting fra databasen, FETCH API greier, vetikke finne ut av senere
+        //    .then(({ data }) => {
+        this.setState({
+            results: ['hei'] //data.data?
+        });
+        console.log("results: " + this.state.results);
+    }
+
+    saveQuery(){
+        let queries = [...this.state.previousQueries];   //creating the copy
+        let id = this.state.queryId + 1;
+        //adding new data
+        queries.push({
+            id: id,
+            query: this.state.query
+        });
+
+        //updating the state value
+        this.setState({previousQueries: queries, queryId: id});
     }
 
     handleSubmit(event) {
         //gjøre noe her med input
-        alert('A name was submitted: ' + this.state.value);
+        console.log(this.state.query);
         event.preventDefault();
-    }
+        this.saveQuery();
+        console.log(this.state.previousQueries);
+        this.getInfo();
+        this.setState({query: ''});
 
+    }
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                Search:
+            <div>
+            <form onSubmit={this.handleSubmit} style={{display: 'block'}}>
+                <h3>Search</h3>
                 <label>
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    <input type="text" value={this.state.query} onChange={this.handleChange} />
                 </label>
                 <input type="submit" value="Submit" />
             </form>
+            </div>
         );
     }
 }
