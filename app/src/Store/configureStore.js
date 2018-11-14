d6acb404d9bc514db309b819664155d9d5014480
mortefal <morten_falstad@hotmail.com>
2018-11-14 17:06:58 +0100

    Added some lines to configureStore

--- app/src/Store/configureStore.js
+++ app/src/Store/configureStore.js
@@ -1,6 +1,14 @@
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
+import {createLogger} from "redux-logger";
+import thunkMiddleware from 'redux-thunk'
+import rootReducer from "../Reducers/reducers";
 
+const loggerMiddleware = createLogger();
 
 export default function configureStore(initialState){
-    return createStore();
+    return createStore(
+        rootReducer,
+        initialState,
+        applyMiddleware(thunkMiddleware, loggerMiddleware)
+    );
 }
